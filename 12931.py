fbc8dc25bd53421d97e4d21ce2ccf383f407063b
han-mg <alsrb1277@gmail.com>
2024-06-22 22:38:33 +0900

    24_06_22

---
+++ 12931.py
@@ -0,0 +1,18 @@
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+
+cnt = 0
+
+while True:
+    for i in range(n):
+        if a[i] % 2 == 1:
+            a[i] -= 1
+            cnt += 1
+    if sum(a) == 0:
+        break
+    for i in range(n):
+        if a[i] % 2 == 0:
+            a[i] /= 2
+    cnt += 1
+print(cnt)
