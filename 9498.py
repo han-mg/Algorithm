f93c49e67459b58e6f85947bf162ca18b5270a4b
han-mg <alsrb1277@gmail.com>
2024-06-23 23:28:06 +0900

    24_06_23

---
+++ 9498.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if 90 <= n & n <= 100:
+    print("A")
+elif 80 <= n & n <= 89:
+    print("B")
+elif 70 <= n & n <= 79:
+    print("C")
+elif 60 <= n & n <= 69:
+    print("D")
+else:
+    print("F")
